95d6d365362fa40c7d209d0ce9700d9ddd5fa65b
s6210110342 <6210110342@psu.ac.th>
2021-07-05 11:50:48 +0700

    part 6

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState }from 'react';
+import _ from 'lodash';
+
 import CharacterCard from './CharacterCard';
+import { useState } from 'react';
+
+const prepareStateFromWord = given_word => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars
+    }
+}
+
 export default function WordCard(props){
+
+    const [state, setState] = useState(prepareStateFromWord(props.value))
 
     const activationHandler = c => {
         console.log(`${c} has been activated`)
