bdfe9e9f38603da9511afd4879bf21c1c1dfd919
s6210110342 <6210110342@psu.ac.th>
2021-07-05 23:06:19 +0700

    r

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,28 +1,28 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
 export default function CharacterCard(props) {
+  const [active, setActive] = useState(false);
+  const attemptRef = useRef(props.attempt);
 
-    const [active, setActive] = useState(false);
-    const attemptRef = useRef(props.attempt);
-
-    const activate = () => {
-        if(!active){
-            setActive(true)
-            props.activationHandler(props.value)
-        }
+  const activate = () => {
+    if (!active) {
+      setActive(true);
+      props.activationHandler(props.value);
     }
+  };
 
-    useEffect(() => {
-        if(attemptRef.current != props.attempt){
-            setActive(false);
-            attemptRef.current = props.attempt
-        }
-    })
-    
-    const className = `card ${active ? 'activeCard': ''}`
-    
-    return (
-        <div className={className} onClick={activate}>{props.value}</div>
-    )
+  useEffect(() => {
+    if (attemptRef.current != props.attempt) {
+      setActive(false);
+      attemptRef.current = props.attempt;
+    }
+  });
 
+  const className = `card ${active ? "activeCard" : ""}`;
+
+  return (
+    <div className={className} onClick={activate}>
+      {props.value}
+    </div>
+  );
 }
